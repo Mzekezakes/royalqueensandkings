1fc1c3fa80ffec610beacda63d3b88870f8f82f8
kingvicky23 <atlehangsejake@gmail.com>
2023-05-03 12:58:28 +0200

    login

--- login.js
+++ login.js
@@ -1,7 +1,26 @@
-function Login() {
-    var user =
-    document.getElementById ('username').value;
-    var pass =
-    document.getElementById ("password").value;
-    if 
+//login function
+
+function Loginform () {
+    //Get form data
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    //Retrieve login data from session storage
+    var savedUsername = sessionStorage.getItem("username");
+    var savedPassword = sessionStorage.getItem("password");
+
+    //Check if login data matches form data
+    if (username === savedUsername && password === savedPassword) {
+        //Login successful
+        alert("Login successful.");
+        //Redirect to landing page
+        window.location.href = "index.html";
+    } else {
+        //Login failed
+        alert("Invalid username or password");
+    }
+
+    //Reset form
+    document.getElementById("loginform").reset();
+    
 }
