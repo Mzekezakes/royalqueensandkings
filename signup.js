29c79c9c0258207b5eeb76b76dd253cb2bcc902e
KMmofokeng <mofokengkarabo49@gmail.com>
2023-05-17 13:15:47 +0200

    signup

--- signup.js
+++ signup.js
@@ -1,7 +1,7 @@
 //signup fuction
 
 function Signup() {
-var userId = document.getElementById("user Id").value;
+var userId = document.getElementById("userId").value;
 var name = document.getElementById("name").value;
 var email = document.getElementById("email").value;
 var phone = document.getElementById("contact").value;
@@ -45,7 +45,7 @@ if (status === "") {
 }
 
 // Save form data in session memory
-sessionStorage.setItem("user Id",userId);
+sessionStorage.setItem("userId",userId);
 sessionStorage.setItem("name",name);
 sessionStorage.setItem("email",email);
 sessionStorage.setItem("phone",phone);
@@ -54,6 +54,15 @@ sessionStorage.setItem("location",location);
 sessionStorage.setItem("password",password);
 sessionStorage.setItem("status",status);
 
+document.getElementById("userId").innerHTML=userID;
+document.getElementById("Name").innerHTML=Name;
+document.getElementById("email").innerHTML=email;
+document.getElementById("phone").innerHTML=phone;
+document.getElementById("profession").innerHTML=profession;
+document.getElementById("location").innerHTML=location;
+document.getElementById("password").innerHTML=password;
+document.getElementById("status").innerHTML=status;
+
 //Redirect to landing page
 window.location.href = "index.html";
 
@@ -61,3 +70,12 @@ window.location.href = "index.html";
 alert("Signup successful.");
 }
 
+//function Userprofile() {
+// // Display user details in session memory
+// document.getElementById("User ID").innerHTML = User ID;
+// document.getElementById("Name").innerHTML = Name;
+// document.getElementById("Email Address").innerHTML = Email Address;
+// document.getElementById("Phone").innerHTML = Phone;
+// document.getElementById("Profession").innerHTML = Profession;
+// document.getElementById("Location").innerHTML = Location;
+// document.getElementById("Status").innerHTML = Status;}
